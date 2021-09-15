0feb2c009fbe32a4187dad0273ad409214607aeb
AminudinKahar(GitHub) <aminudinkahar@gmail.com>
2021-09-16 06:24:43 +0800

    commit css, img
    - include css in website
    - include img folder with logo

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,10 +3,14 @@
 @section('content')
 
     <main role="main" class="inner cover">
-        <h1 class="cover-heading">This is Contact.</h1>
-        <p class="lead">AM-Code is my official website. Feel free to explore deeper and leave any comment.</p>
-        <p class="lead">
-            <a href="#" class="btn btn-lg btn-secondary">Learn more</a>
-        </p>
+        <div class="title-slideup">
+            <h1 class="cover-heading">This is Contact Page.</h1>
+        </div>
+        <div class="par-slideup">
+            <p class="lead">AM-Code is my official website. Feel free to explore deeper and leave any comment.</p>
+        </div>
+        <div class="btn-slideup">
+            <button href="#" class="btn btn-lg btn-grad-success">Learn more</button>
+        </div>
     </main>
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,13 +3,16 @@
 @section('content')
 
     <main role="main" class="inner cover">
-        <h1 class="cover-heading">This is Me.</h1>
-        <p class="lead">AM-Code is my official website. Feel free to explore deeper and leave any comment.</p>
-        <p class="lead">
-            <a href="#" class="btn btn-lg btn-secondary">Learn more</a>
-        </p>
+        <div class="title-slideup">
+            <h1 class="cover-heading">This is Me.</h1>
+        </div>
+        <div class="par-slideup">
+            <p class="lead">AM-Code is my official website. Feel free to explore deeper and leave any comment.</p>
+        </div>
+        <div class="btn-slideup">
+            <button href="#" class="btn btn-lg btn-grad-success">Learn more</button>
+        </div>
+       
     </main>
-    <div class="container-fluid">
-    </div>
     
 @endsection
