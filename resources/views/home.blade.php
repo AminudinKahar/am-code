1941c54723aa1496f2563b142a18c023f9b78e9d
AminudinKahar(GitHub) <aminudinkahar@gmail.com>
2021-09-21 16:20:51 +0800

    update master.blade.php
    include dark mode in master.blade.php
    fixed css, js in the project

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,13 +4,13 @@
 
     <main role="main" class="inner cover">
         <div class="title-slideup">
-            <h1 class="cover-heading">This is Me.</h1>
+            <h1 class="cover-heading">Hello!</h1>
         </div>
         <div class="par-slideup">
             <p class="lead">AM-Code is my official website. Feel free to explore deeper and leave any comment.</p>
         </div>
         <div class="btn-slideup">
-            <button href="#" class="btn btn-lg btn-grad-success">Learn more</button>
+            <button class="btn btn-lg btn-grad-success">Learn more</button>
         </div>
        
     </main>
